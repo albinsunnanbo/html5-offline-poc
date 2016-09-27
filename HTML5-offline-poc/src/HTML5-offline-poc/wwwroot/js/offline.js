976003157dd7a432670158d930ae45227671d3ce
Albin Sunnanbo <albin@sunnanbo.com>
2016-09-27 14:34:39 +0200

    Test offline storage

---
+++ HTML5-offline-poc/src/HTML5-offline-poc/wwwroot/js/offline.js
@@ -0,0 +1,33 @@
+﻿// Write your Javascript code.
+$(function () {
+    "use strict";
+    var storageKey = "offlineContent";
+
+    if (!localStorage[storageKey]) {
+        localStorage[storageKey] = JSON.stringify([]);
+    }
+
+    var fillListFromLocalStorage = function () {
+        var notes = JSON.parse(localStorage[storageKey]);
+
+        var list = $("#offline-items");
+        list.empty();
+        notes.forEach(function (value, idx) {
+            list.append("<li>" + value + "</li>");
+        });
+    };
+    fillListFromLocalStorage();
+
+    $("#btn-add").click(function () {
+        var field = $("#txt-new-data");
+        var value = field.val();
+
+        var notes = JSON.parse(localStorage[storageKey]);
+        notes.push(value);
+        localStorage[storageKey] = JSON.stringify(notes);
+
+        fillListFromLocalStorage();
+
+        field.val("");
+    })
+});
